62f6c466fe0da8f8ea555e9a01461725c700e52d
Nick Rinaldi <39104732+nickrinaldi88@users.noreply.github.com>
2020-12-29 18:45:15 -0500

    Add files via upload

--- app2.js
+++ app2.js
@@ -24,6 +24,7 @@ d3.csv(
     .data(Object.keys(current_data))
     .enter()
     .append("option")
+    // .text("Select a word")
     .text(function (d) {
       return d;
     });
@@ -52,7 +53,7 @@ d3.csv(
     // .attr("height", height + margin.top + margin.bottom)
     .attr("viewBox", "0 0 1400 800")
     .append("g")
-    .attr("transform", "translate(40, 20)")
+    .attr("transform", "translate(80, 20)")
     .classed("svg-content-responsive", true);
   // .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
@@ -64,14 +65,29 @@ d3.csv(
   svg
     .append("g")
     .attr("transform", "translate(0," + height + ")")
-    .attr("class", "myXaxis");
+    .attr("class", "myXaxis")
+    .append("text")
+    .attr("class", "axis-label-x")
+    .attr("x", "50%")
+    .attr("dy", "4em")
+    .style("fill", "black")
+    .text("Months");
   // .text("Months");
 
   // Initialize an Y axis
   var y = d3.scaleLinear().range([height, 0]);
 
   var yAxis = d3.axisLeft().scale(y);
-  svg.append("g").attr("class", "myYaxis");
+  svg
+    .append("g")
+    .attr("class", "myYaxis")
+    .append("text")
+    .attr("class", "axis-label-y")
+    .attr("x", "-10%")
+    .attr("transform", "rotate(-90)")
+    .attr("dy", "-7%")
+    .style("fill", "black")
+    .text("Word Count");
 
   // Create a function that takes a dataset as input and update the plot:
 
@@ -123,9 +139,9 @@ d3.csv(
   update(current_data.Obama);
 });
 
-d3.csv("bb_totals.csv").then(function (csv) {
-  console.log(csv);
+// bottom graph
 
+d3.csv("bb_totals.csv").then(function (csv) {
   var margin = {
       top: 20,
       right: 30,
@@ -134,6 +150,16 @@ d3.csv("bb_totals.csv").then(function (csv) {
     },
     width = 1200 - margin.left - margin.right,
     height = 400 - margin.top - margin.bottom;
+
+  d3.select("#slct2")
+    .selectAll("option")
+    .data(["Trump", "No Trump"])
+    .enter()
+    .append("option")
+    // .text("Select a word")
+    .text(function (d) {
+      return d;
+    });
 
   var svg2 = d3
     .select("#sec-div")
@@ -144,20 +170,45 @@ d3.csv("bb_totals.csv").then(function (csv) {
     .attr("viewBox", "0 0 1400 800")
     .append("g")
     // .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-    .attr("transform", "translate(40, 20)")
+    .attr("transform", "translate(100, 20)")
     .classed("svg-content-responsive", true);
 
-  var x = d3.scaleLinear().domain([0, 40000]).range([0, width]);
+  // var the_option = console.log(document.getElementById("slct2").value);
+
+  d3.select("#slct2").on("change", function () {
+    var the_option = document.getElementById("slct2").value;
+    console.log(the_option);
+    if (the_option === "No Trump") {
+      noTrump();
+    }
+
+    // accept noTrump which contains non Trump data, or Trump which contains Trump data
+  });
 
   // console.log(d3.max(csv, (d) => d.Count));
+
+  // x axis
+
+  var x = d3.scaleLinear().domain([0, 40000]).range([0, width]);
 
   svg2
     .append("g")
     .attr("transform", "translate(0," + height + ")")
+    .attr("class", "myXaxis2")
     .call(d3.axisBottom(x))
     .selectAll("text")
     .attr("transform", "translate(-10, 0)rotate(-45)")
     .style("text-anchor", "end");
+
+  svg2
+    .append("text")
+    .attr("class", "axis-label-x")
+    .attr("x", "40%")
+    .attr("dy", "55%")
+    .style("fill", "black")
+    .text("Total Word Count");
+
+  // y axis
 
   var y = d3
     .scaleBand()
@@ -168,9 +219,20 @@ d3.csv("bb_totals.csv").then(function (csv) {
       })
     )
     .padding(5);
-  svg2.append("g").call(d3.axisLeft(y));
 
-  // lines
+  svg2
+    .append("g")
+    .attr("class", "myYaxis2")
+    .call(d3.axisLeft(y))
+    .append("text")
+    .attr("class", "axis-label-y")
+    .attr("x", "-10%")
+    .attr("transform", "rotate(-90)")
+    .attr("dy", "-8%")
+    .style("fill", "black")
+    .text("Words");
+
+  // lines;
   svg2
     .selectAll("myline")
     .data(csv)
@@ -203,26 +265,126 @@ d3.csv("bb_totals.csv").then(function (csv) {
     .style("fill", "#69b3a2")
     .attr("stroke", "black");
 
-  // create select option, with trump and without trump
-  // create function that changes x and y values with and without trumps addition
+  function noTrump() {
+    d3.csv("notrump.csv").then(function (data) {
+      var margin = {
+          top: 20,
+          right: 30,
+          bottom: 30,
+          left: 0,
+        },
+        width = 1200 - margin.left - margin.right,
+        height = 400 - margin.top - margin.bottom;
 
-  // d3.select("select")
-  //   .selectAll("option")
-  //   .data(Object.keys(current_data))
-  //   .enter()
-  //   .append("option")
-  //   .text(function (d) {
-  //     return d;
-  //   });
+      // var svg2 = d3
+      //   .select("#sec-div")
+      //   .classed("svg-container", true)
+      //   .append("svg")
+      //   // .attr("width", width + margin.left + margin.right)
+      //   // .attr("height", height + margin.top + margin.bottom)
+      //   .attr("viewBox", "0 0 1400 800")
+      //   .append("g")
+      //   // .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+      //   .attr("transform", "translate(100, 20)")
+      //   .classed("svg-content-responsive", true);
+
+      var x = d3.scaleLinear().domain([0, 14000]).range([0, width]);
+
+      // console.log(d3.max(csv, (d) => d.Count));
+
+      svg2
+        .selectAll(".myXaxis2")
+        .transition()
+        .duration(3000)
+        .call(d3.axisBottom(x));
+
+      svg2
+        .append("text")
+        .attr("class", "axis-label-x")
+        .attr("x", "40%")
+        .attr("dy", "55%")
+        .style("fill", "black")
+        .text("Total Word Count");
+
+      var y = d3
+        .scaleBand()
+        .range([0, height])
+        .domain(
+          data.map(function (d) {
+            return d.Word;
+          })
+        )
+        .padding(5);
+
+      svg2
+        .selectAll(".myYaxis2")
+        .transition()
+        .duration(3000)
+        .call(d3.axisLeft(y));
+
+      // lines
+
+      var u2 = svg2.selectAll(".myline").data(data);
+
+      u2.enter()
+        .append("line")
+        .attr("class", "myline")
+        .merge(u2)
+        .transition()
+        .duration(3000)
+        .attr("x1", function (d) {
+          return x(d.Count);
+        })
+        .attr("x2", x(0))
+        .attr("y1", function (d) {
+          return y(d.Word);
+        })
+        .attr("y2", function (d) {
+          return y(d.Word);
+        })
+        .attr("fill", "none")
+        .attr("stroke", "grey");
+
+      var u2_circ = svg2.selectAll(".mycircle").data(data);
+
+      u2_circ
+        .enter()
+        .append("circle")
+        .attr("class", "mycircle")
+        .merge(u2)
+        // .data(csv)
+        // .enter()
+        .transition()
+        .duration(3000)
+        .attr("cx", function (d) {
+          return x(d.Count);
+        })
+        .attr("cy", function (d) {
+          return y(d.Word);
+        })
+        .attr("r", "4")
+        .style("fill", "#69b3a2")
+        .attr("stroke", "black");
+
+      // create select option, with trump and without trump
+      // create function that changes x and y values with and without trumps addition
+
+      // d3.select("select")
+      //   .selectAll("option")
+      //   .data(Object.keys(current_data))
+      //   .enter()
+      //   .append("option")
+      //   .text(function (d) {
+      //     return d;
+      //   });
+    });
+  }
 });
 
-// TODO:
-// 1. resize our graph
-// 1a. move over y axis
-// 2. color and stylize
-//      - Add a background opacity
-//      - Change font
-//
+// Current Problem:
+// 1. When "No Trump" is selected, our lines are drawn.
+// 2.
+// 3.
 
 // http://learnjsdata.com/read_data.html
 
