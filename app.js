be642c011767d5dde0b102c82b1b86ed0f3374cc
Nick Rinaldi <39104732+nickrinaldi88@users.noreply.github.com>
2021-06-08 10:25:47 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 d3.csv(
-  "https://raw.githubusercontent.com/nickrinaldi88/BreitBart_DataVis/main/breitbartData.csv"
+  "https://raw.githubusercontent.com/nickrinaldi88/Breitbart-Words-Data-Visualization/master/breitbartData.csv"
 ).then(function (csv) {
   // reformat data
   //   console.log(csv);
